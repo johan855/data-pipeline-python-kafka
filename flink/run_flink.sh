1a7f5b459e8e52529dcb7ecaa3615e1b112e33d4
johan855 <johan855@gmail.com>
2019-09-24 16:41:45 +0200

    Create run_flink.sh

---
+++ flink/run_flink.sh
@@ -0,0 +1,3 @@
+docker network create flink-net
+docker run --name flink-master --net flink-net -e ENABLE_INIT_DAEMON=false -d bde2020/flink-master:1.7.2-hadoop2.8
+docker run --name flink-worker --net flink-net -e ENABLE_INIT_DAEMON=false -e FLINK_MASTER_PORT_6123_TCP_ADDR=flink-master -d bde2020/flink-worker:1.7.2-hadoop2.8
